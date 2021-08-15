3f2230264897f8add8c47040bd65ef2528f4664b
Patipark Sopha <iamwad@gmail.com>
2021-08-15 17:25:04 +0700

    แก้ไขโครงสร้างไฟล์ migration และทำการ migrate เพื่อสร้างตาราง products

--- backend/database/migrations/2021_08_15_094227_create_products_table.php
+++ backend/database/migrations/2021_08_15_094227_create_products_table.php
@@ -14,7 +14,11 @@ class CreateProductsTable extends Migration
     public function up()
     {
         Schema::create('products', function (Blueprint $table) {
-            $table->id();
+            $table->increments('id');
+            $table->string('name');
+            $table->string('slug');
+            $table->string('description')->nullable();
+            $table->decimal('price',9, 2); // 2,859,893.50
             $table->timestamps();
         });
     }
