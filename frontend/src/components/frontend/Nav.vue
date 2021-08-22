fbb58eebbf6aae0e08ad3869b74245e72bccb0fc
Patipark Sopha <iamwad@gmail.com>
2021-08-22 20:29:05 +0700

    ใส่ menu router-link ไปยัง Home,About

--- frontend/src/components/frontend/Nav.vue
+++ frontend/src/components/frontend/Nav.vue
@@ -1,5 +1,12 @@
 <template>
-    <h1>Menu</h1>
+    <ul>
+        <li>
+            <router-link to="/">Home</router-link>
+        </li>
+        <li>
+            <router-link to="/about">About</router-link>
+        </li>
+    </ul>
 </template>
 
 <script>
