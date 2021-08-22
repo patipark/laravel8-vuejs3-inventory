0bb401d3f2eb71b8342c3abcba1d622304879f4e
Patipark Sopha <iamwad@gmail.com>
2021-08-22 19:50:38 +0700

    ที่ router ให้ import FrontendLayout มา และทำการปรับแต่ง route ใหม่ โดยให้ FrontendLayout เป็น component หลัก และเอา Home,About ไปเป็น children ใน route เดิม

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
+//Import Layouts
+import FrontendLayout from '../layouts/Frontend.vue'
+
 import Home from '../views/Home.vue'
 import About from '../views/About.vue'
 
@@ -6,16 +10,24 @@ const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Home,
+      }
+    ]
   },
   {
     path: '/about',
     name: 'About',
-    component: About
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: About,
+      }
+    ]
   }
 ]
 
