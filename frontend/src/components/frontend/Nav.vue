0f1d2ac4555ae9a680a571a41ce88a30477abac2
Patipark Sopha <iamwad@gmail.com>
2021-08-22 19:48:29 +0700

    สร้าง FrontendLayout และ Nav เมนูกับ Footer และเอามารวมกัน

---
+++ frontend/src/components/frontend/Nav.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1>Menu</h1>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
---
+++ frontend/src/layouts/Frontend.vue
@@ -0,0 +1,23 @@
+<template>
+    <NavBar />
+    <main>
+        <router-view></router-view>
+    </main>
+    <Footer />
+</template>
+
+<script>
+import NavBar from '../components/frontend/Nav.vue';
+import Footer from '../components/frontend/Foot.vue';
+export default {
+    name: 'FrontendLayout',
+    components: {
+        NavBar,
+        Footer
+    }
+}
+</script>
+
+
+
+
