ef5380deca5a960a51f60d4fbd250ca4978cbb31
Patipark Sopha <iamwad@gmail.com>
2021-08-22 21:07:40 +0700

    ปรับแต่งการเรียก app ใน main.css ไว้ใจตัวแปรชื่อ app

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,4 +4,7 @@ import router from './router'
 import store from './store'
 import '@/styles/tailwind.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.mount('#app')
