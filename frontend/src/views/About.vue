29d47505a8a0990ffcf3f9cf2cd936a0b8489178
Patipark Sopha <iamwad@gmail.com>
2021-08-22 21:14:01 +0700

    ทดลองใช้ class tailwind css ใน หน้า About

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,7 +1,9 @@
 <template>
+<div class="justify-center flex items-center bg-green-500 h-full">
   <h1>About Page</h1>
   <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quam quo amet quasi eligendi. Magni, quas tempora praesentium voluptatibus explicabo harum sint! Porro dolorum blanditiis deserunt, atque ipsa ea repudiandae?
   </p>
+</div>
 </template>
 
