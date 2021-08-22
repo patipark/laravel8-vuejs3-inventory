c03d96c4b6dff60502248cbd70b9fa5a23b4eaf5
Patipark Sopha <iamwad@gmail.com>
2021-08-22 21:11:41 +0700

    ทดลองใช้ class css ของ tailwind ที่หน้า home

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,10 @@
 <template>
+<div class="justify-center flex items-center bg-blue-500 h-full">
   <h1>Home Page</h1>
   <p>
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad unde sint alias, nesciunt, quas ut dolorum optio mollitia vero eligendi recusandae esse laborum ducimus totam cum nihil rem. Ea, ipsam.
   </p>
+
+</div>
 </template>
 
