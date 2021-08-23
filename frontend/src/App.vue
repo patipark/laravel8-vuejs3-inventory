1e63ec41b414aeefb6ebb7ea35c12bd9f95662c3
Patipark Sopha <iamwad@gmail.com>
2021-08-23 14:17:34 +0700

    ใส่ brand page ให้ต่อท้าย page title เสมอ

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,11 +3,12 @@
 </template>
 
 <script>
+let brand = " | Inventory Vue SPA" //ใส่ brand page 
 export default {
   name: "App",
   watch: {
     $route(to) {
-      (document.title = to.meta.title ),
+      (document.title = to.meta.title + brand ),
         (document.description = to.meta.description);
     },
   },
