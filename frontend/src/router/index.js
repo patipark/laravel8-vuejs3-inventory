fae06a43f729c9487eb8c237da4e6f7533dddcf4
Patipark Sopha <iamwad@gmail.com>
2021-08-23 12:09:31 +0700

    แก้ไขไฟล์ route ให้ import component ต่างๆเข้ามาใน route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,11 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-//Import Layouts
-import FrontendLayout from '../layouts/Frontend.vue'
+// Import Layouts
+import FrontendLayout from '@/layouts/Frontend.vue'
 
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
+// Import Views
+// Frontend
+import Home from '@/views/frontend/Home.vue'
+import About from '@/views/frontend/About.vue'
+import Portfolio from '@/views/frontend/Portfolio.vue'
+import Service from '@/views/frontend/Service.vue'
+import Contact from '@/views/frontend/Contact.vue'
+import Register from '@/views/frontend/Register.vue'
+import Login from '@/views/frontend/Login.vue'
+import ForgotPassword from '@/views/frontend/ForgotPassword.vue'
+import NotFound404 from '@/views/frontend/NotFound404.vue'
 
+// Backend
 const routes = [
   {
     path: '/',
@@ -14,7 +24,7 @@ const routes = [
     children: [
       {
         path: '',
-        component: Home,
+        component: Home
       }
     ]
   },
@@ -25,10 +35,83 @@ const routes = [
     children: [
       {
         path: '',
-        component: About,
+        component: About
       }
     ]
+  },
+  {
+    path: '/portfolio',
+    name: 'Portfolio',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Portfolio
+      }
+    ]
+  },
+  {
+    path: '/service',
+    name: 'Service',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Service
+      }
+    ]
+  },
+  {
+    path: '/contact',
+    name: 'Contact',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Contact
+      }
+    ]
+  },
+  {
+    path: '/register',
+    name: 'Register',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Register
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: Login
+      }
+    ]
+  },
+  {
+    path: '/forgotpassword',
+    name: 'ForgotPassword',
+    component: FrontendLayout,
+    children: [
+      {
+        path: '',
+        component: ForgotPassword
+      }
+    ]
+  },
+
+  // Error 404
+  {
+    path: "/:catchAll(.*)",
+    component: NotFound404,
   }
+
 ]
 
 const router = createRouter({
