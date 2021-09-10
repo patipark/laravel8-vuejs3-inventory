05443ec99200c7fa87b7821dbec5f182b0f0ab4b
Patipark Sopha <iamwad@gmail.com>
2021-09-10 16:56:52 +0700

    เพิ่ม ค่า authService ในส่วน axios withCredentials = true

--- frontend/src/services/AuthService.js
+++ frontend/src/services/AuthService.js
@@ -5,6 +5,7 @@ axios.defaults.withCredentials = false
 const authService = axios.create({
   // baseURL: "http://localhost:8000/api/",
   baseURL: "http://localhost:8001/api/", //ปกติ laravel คำสั่ง php artisan serve จะรัน port 8000 แต่ในที่นี้ port ไม่ว่างเลยใข้ 8001
+  withCredentials: true,
   headers: {
     "Content-type": "application/json",
     "Accept": "application/json"
